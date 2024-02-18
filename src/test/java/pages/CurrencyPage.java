798db915fec37ae5bd54e426f615c49217f2da5c
Maxim-Kazliakouski <maxim.kazliakouski@gmail.com>
2024-02-18 12:55:40 +0300

    fix(): fix

--- src/test/java/pages/CurrencyPage.java
+++ src/test/java/pages/CurrencyPage.java
@@ -41,10 +41,10 @@ public class CurrencyPage extends BasePage {
         String forecast;
         if (forecastPositiveOrNegative.contains("positive")) {
 //            forecast = (format("%s will grow%n", currency));
-            forecast = (format("%s will grow\n", currency));
+            forecast = (format("%s will grow" + "\n", currency));
         } else {
 //            forecast = (format("%s will fall%n", currency));
-            forecast = (format("%s will fall\n", currency));
+            forecast = (format("%s will fall" + "\n", currency));
         }
         try {
             FileWriter writer = new FileWriter("currency.txt", true);
