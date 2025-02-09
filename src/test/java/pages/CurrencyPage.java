e4dcfcd55e8648997b8d0d162ac7de5415fcc468
Max Kazliakouski <MKozlyakovskiy@alfabank.ru>
2025-02-09 20:29:13 +0300

    fix(): beatify

--- src/test/java/pages/CurrencyPage.java
+++ src/test/java/pages/CurrencyPage.java
@@ -48,7 +48,7 @@ public class CurrencyPage extends BasePage {
         try {
             FileWriter writer = new FileWriter("currency_" + currency + ".txt", true);
 //            writer.write("\"" + currency + " --> " + currencyText + forecast + "\"");
-            writer.write(currency + " --> " + currencyText);
+            writer.write("THE BEST " + action.toUpperCase() + " OF " + currency + " --> " + currencyText);
 //            writer.write(currency + "_" + currencyText);
 //            writer.write(forecast + "                 ");
 //            writer.write(forecast);
